160ce91991322d2ea511f0d988948aa61ea2e54e
sc0Vu <alk03073135@gmail.com>
2024-11-14 15:31:43 +0800

    zonda: add watchTicker

---
+++ ts/src/pro/zonda.ts
@@ -0,0 +1,152 @@
+//  ---------------------------------------------------------------------------
+
+import zondaRest from '../zonda.js';
+import Client from '../base/ws/Client.js';
+import { ExchangeError } from '../base/errors.js';
+import { Dict, Ticker } from '../base/types.js';
+
+//  ---------------------------------------------------------------------------
+
+export default class zonda extends zondaRest {
+    describe () {
+        return this.deepExtend (super.describe (), {
+            'has': {
+                'ws': true,
+                'watchOHLCV': false,
+                'watchOrderBook': false,
+                'watchTicker': true,
+                'watchTickers': false,
+                'watchTrades': false,
+                'watchTradesForSymbols': false,
+                'watchBalance': false,
+                'watchOrders': false,
+                'watchMyTrades': false,
+                'watchPositions': false,
+            },
+            'urls': {
+                'api': {
+                    'ws': 'wss://api.zondacrypto.exchange/websocket',
+                },
+            },
+            'options': {
+                'tradesLimit': 1000,
+                'ordersLimit': 1000,
+                'OHLCVLimit': 1000,
+            },
+            'streaming': {
+                'keepAlive': 20000,
+                'ping': this.ping,
+            },
+        });
+    }
+
+    async watchPublic (topic, messageHash, params = {}) {
+        await this.loadMarkets ();
+        const url = this.urls['api']['ws'];
+        const request: Dict = {
+            'action': 'subscribe-public',
+            'module': 'trading',
+            'path': topic,
+        };
+        const message = this.extend (request, params);
+        return await this.watch (url, messageHash, message, messageHash);
+    }
+
+    async watchTicker (symbol: string, params = {}): Promise<Ticker> {
+        /**
+         * @method
+         * @name zonda#watchTicker
+         * @description watches a price ticker, a statistical calculation with the information calculated over the past 24 hours for a specific market
+         * @see https://docs.zondacrypto.exchange/reference/ticker-2
+         * @param {string} symbol unified symbol of the market to fetch the ticker for
+         * @param {object} [params] extra parameters specific to the exchange API endpoint
+         * @returns {object} a [ticker structure]{@link https://docs.ccxt.com/#/?id=ticker-structure}
+         */
+        await this.loadMarkets ();
+        const market = this.market (symbol);
+        symbol = market['symbol'];
+        const topic = 'ticker/' + market['id'];
+        const messageHash = 'ticker:' + symbol;
+        return await this.watchPublic (topic, messageHash, params);
+    }
+
+    handleTicker (client: Client, message) {
+        //
+        // {
+        //     "action": "push",
+        //     "topic": "trading/ticker/sol-usdc",
+        //     "message": {
+        //         "market": {
+        //             "code": "SOL-USDC",
+        //             "first": {
+        //                 "currency": "SOL",
+        //                 "minOffer": "0.0021",
+        //                 "scale": 8
+        //             },
+        //             "second": {
+        //                 "currency": "USDC",
+        //                 "minOffer": "0.441",
+        //                 "scale": 6
+        //             },
+        //             "amountPrecision": 8,
+        //             "pricePrecision": 6,
+        //             "ratePrecision": 6
+        //         },
+        //         "time": "1731567778864",
+        //         "highestBid": "215.818309",
+        //         "lowestAsk": "217.545891",
+        //         "rate": "210.925539",
+        //         "previousRate": "214.93681"
+        //     },
+        //     "timestamp": "1731567778864",
+        //     "seqNo": 11144256
+        // }
+        //
+        const data = this.safeDict (message, 'message', {});
+        const parsedTicker = this.parseTicker (data);
+        const symbol = parsedTicker['symbol'];
+        this.tickers[symbol] = parsedTicker;
+        const messageHash = 'ticker:' + symbol;
+        client.resolve (parsedTicker, messageHash);
+    }
+
+    ping (client: Client) {
+        return {
+            'action': 'ping',
+        };
+    }
+
+    handlePong (client: Client, message) {
+        //
+        // {
+        //     'action': 'pong',
+        // }
+        //
+        client.lastPong = this.milliseconds ();
+        return message;
+    }
+
+    handleMessage (client: Client, message) {
+        const error = this.safeString (message, 'error');
+        if (error !== undefined) {
+            throw new ExchangeError (this.id + ' ' + error);
+        }
+        const action = this.safeString (message, 'action');
+        if (action === 'pong') {
+            this.handlePong (client, message);
+            return;
+        }
+        const topic = this.safeString (message, 'topic');
+        if (topic !== undefined) {
+            const part = topic.split ('/');
+            const channels = this.safeString (part, 1);
+            const methods: Dict = {
+                'ticker': this.handleTicker,
+            };
+            const exacMethod = this.safeValue (methods, channels);
+            if (exacMethod !== undefined) {
+                exacMethod.call (this, client, message);
+            }
+        }
+    }
+}
